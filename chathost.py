20c6cdc26c970269e71682ee5ba1a0aff2520e3c
mike-agam <91118061+mike-agam@users.noreply.github.com>
2021-09-24 05:17:33 -0500

    Update and rename chatserver.py to chathost.py
    'Server' is already a term that describes the Python and Javascript relationship with Flask. It has been renamed to "host" to prevent any confusion.

--- chathost.py
+++ chathost.py
@@ -5,7 +5,7 @@ from user import User
 # This file can be run to host your own Decent chatroom.
 if __name__ == "__main__":
     user = User("server")
-    print("Starting server on " + user.getAddress())
+    print("Now hosting a Decent chatroom on " + user.getAddress())
     while True:
         if(len(user.receiveMessage()) == 0):
             time.sleep(15)
